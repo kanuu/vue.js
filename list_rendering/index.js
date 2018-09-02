e22097237cad71d72c7e12142cca1682035d1c85
kanuu <123kanuu@gmail.com>
2018-09-02 17:57:48 +0900

    Displaying Filtered/Sorted Results

--- list_rendering/index.js
+++ list_rendering/index.js
@@ -37,3 +37,17 @@ let vm = new Vue({
     }
   }
 })
+
+let numbers = new Vue({
+  el: '#numbers',
+  data: {
+    numbers: [ 1, 2, 3, 4 ,5 ]
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.numbers.filter(function (number) {
+        return number % 2 === 1
+      })
+    }
+  }
+})
