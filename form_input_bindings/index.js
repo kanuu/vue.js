f56c4c1e9167d5b2f4f72c9e8c4fa258f7cca6fa
kanuu <123kanuu@gmail.com>
2018-09-03 00:18:42 +0900

    fix

--- form_input_bindings/index.js
+++ form_input_bindings/index.js
@@ -1,5 +1,19 @@
 new Vue({
-  el: '#example-3',
+  el: '#message',
+  data: {
+    message: ''
+  }
+})
+
+new Vue({
+  el: '#multiline',
+  data: {
+    message: ''
+  }
+})
+
+new Vue({
+  el: '#check_box',
   data: {
     checkedNames: []
   }
