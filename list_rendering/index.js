8301bb79425d94df8cbabcda34a92b8eac44fd0b
kanuu <123kanuu@gmail.com>
2018-09-02 18:56:57 +0900

    wip

--- list_rendering/index.js
+++ list_rendering/index.js
@@ -66,3 +66,44 @@ new Vue({
     ]
   }
 })
+
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">Remove</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+new Vue({
+  el: '#todo-list',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: '皿洗い'
+      },
+      {
+        id: 2,
+        title: 'ゴミ捨て'
+      },
+      {
+        id: 3,
+        title: '芝刈り'
+      }
+    ],
+    nextTodoId: 4
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+})
